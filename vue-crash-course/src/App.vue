c431181fd9101d4efa91088fac4da63d828b2b3a
kale <18kmkf@queensu.ca>
2026-02-12 21:37:33 -0500

    feat: Pivot to using composition API

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -1,26 +1,27 @@
 <script>
 export default {
-  data() {
-    return {
-      name: 'Kaleigh Feder',
-      status: 'pending',
-      tasks: ['Task 1', 'Task 2', 'Task 3'],
-      link: 'https://www.google.com/'
-    }
-  },
+  setup() {
+    const name = "Kaleigh Feder";
+    const status = "pending";
+    const tasks = ["Task 1", "Task 2", "Task 3"];
 
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending';
-      } else if (this.status === 'pending') {
+    const toggleStatus = () => {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
         this.status = "inactive";
       } else {
-        this.status = "active"
+        this.status = "active";
       }
-    }
-  }
-}
+    };
+    return {
+      name,
+      status,
+      tasks,
+      toggleStatus,
+    };
+  },
+};
 </script>
 
 <template>
@@ -31,9 +32,8 @@ export default {
 
   <h3>Tasks</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{task}}</li>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
-  <a v-bind:href="link">Click for google</a>
 
   <button v-on:click="toggleStatus">Change Status</button>
 </template>
