d5f1c40a436561392404c0ee738e8186f0f01879
kale <18kmkf@queensu.ca>
2026-02-12 21:30:53 -0500

    feat: Options API: directives and methods

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -1,4 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Kaleigh Feder',
+      status: 'pending',
+      tasks: ['Task 1', 'Task 2', 'Task 3'],
+      link: 'https://www.google.com/'
+    }
+  },
+
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = "inactive";
+      } else {
+        this.status = "active"
+      }
+    }
+  }
+}
+</script>
 
 <template>
-  <h1>View Jobs</h1>>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{task}}</li>
+  </ul>
+  <a v-bind:href="link">Click for google</a>
+
+  <button v-on:click="toggleStatus">Change Status</button>
 </template>
