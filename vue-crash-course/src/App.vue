eea2e18a119a44a3ae7ce1b29e0d326a11228f87
kale <18kmkf@queensu.ca>
2026-02-15 14:16:11 -0500

    feat: dynamically add and remove tasks

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -4,6 +4,7 @@ import { ref } from "vue";
 const name = ref("Kaleigh Feder");
 const status = ref("pending");
 const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+const newTask = ref("");
 
 const toggleStatus = () => {
   if (status.value === "active") {
@@ -14,6 +15,17 @@ const toggleStatus = () => {
     status.value = "active";
   }
 };
+
+const addTask = () => {
+  if (newTask.value.trim() != "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask =(index) => {
+  tasks.value.splice(index, 1)
+}
 </script>
 
 <template>
@@ -22,9 +34,18 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="" newTask>Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">submit</button>
+  </form>
+
   <h3>Tasks</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
 
   <button v-on:click="toggleStatus">Change Status</button>
