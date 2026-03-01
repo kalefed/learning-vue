1ed2ac2dc6868205d631d7816603284e11b17e38
kale <18kmkf@queensu.ca>
2026-03-01 12:43:50 -0500

    feat: :tada: Import the theme_files and add in navigation bar component

---
+++ vue-crash-course/src/App copy.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const name = ref("Kaleigh Feder");
+const status = ref("pending");
+const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+const newTask = ref("");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() != "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log("Error fetching tasks");
+  }
+});
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="" newTask>Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">submit</button>
+  </form>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+
+  <button v-on:click="toggleStatus">Change Status</button>
+</template>
