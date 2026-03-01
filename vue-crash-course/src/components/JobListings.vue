5b82216dd6af738ba3d304e8651370d41779d55e
kale <18kmkf@queensu.ca>
2026-03-01 16:26:50 -0500

    feat: Add job listings spinner on load

--- vue-crash-course/src/components/JobListings.vue
+++ vue-crash-course/src/components/JobListings.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, reactive, defineProps, onMounted } from "vue";
+import { PulseLoader } from "vue-spinner";
 import JobListing from "./JobListing.vue";
 import axios from "axios";
 
@@ -33,7 +34,13 @@ onMounted(async () => {
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-600 py-6">
+        <PulseLoader />
+      </div>
+
+      <!-- Show job listing when done loading -->
+      <div v-else class="grid grid-cols-1 gap-6 md:grid-cols-3">
         <JobListing
           v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
