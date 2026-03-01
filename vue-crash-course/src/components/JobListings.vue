4ba49646bdb7b0d2d4f2a7666813698091ef7b46
kale <18kmkf@queensu.ca>
2026-03-01 16:13:51 -0500

    feat: Fetch jobs from json-server with axios

--- vue-crash-course/src/components/JobListings.vue
+++ vue-crash-course/src/components/JobListings.vue
@@ -1,13 +1,22 @@
 <script setup>
-import jobData from "@/jobs.json";
-import { ref, defineProps } from "vue";
+import { ref, defineProps, onMounted } from "vue";
 import JobListing from "./JobListing.vue";
+import axios from "axios";
 
-const jobs = ref(jobData); // similar to useState in react
+const jobs = ref([]); // similar to useState in react
 
 defineProps({
   limit: Number,
   showButton: { type: Boolean, default: false },
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:8000/jobs");
+    jobs.value = response.data;
+  } catch (error) {
+    console.error("Error fetching jobs", error);
+  }
 });
 </script>
 
