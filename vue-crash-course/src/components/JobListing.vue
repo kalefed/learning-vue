6c3befffdfb996d60c46ede07463e906e60ace6a
kale <18kmkf@queensu.ca>
2026-03-01 15:13:32 -0500

    feat: Add dynamic job page to router & fix job/ path to jobs/

--- vue-crash-course/src/components/JobListing.vue
+++ vue-crash-course/src/components/JobListing.vue
@@ -52,7 +52,7 @@ const truncatedDescription = computed(() => {
           {{ job.location }}
         </div>
         <a
-          :href="'/job/' + job.id"
+          :href="'/jobs/' + job.id"
           class="h-9 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
